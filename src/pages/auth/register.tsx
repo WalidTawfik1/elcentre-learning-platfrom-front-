8ff521881828ee022ccd3e908d8314161fbf8524
Walid Tawfik <walidtawfik362@gmail.com>
2025-04-30 20:28:43 +0300

    Remove 'Other' option from gender selection in registration form for improved clarity

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -192,7 +192,6 @@ export default function Register() {
                     <SelectContent>
                       <SelectItem value="male">Male</SelectItem>
                       <SelectItem value="female">Female</SelectItem>
-                      <SelectItem value="other">Other</SelectItem>
                       <SelectItem value="prefer_not_to_say">Prefer not to say</SelectItem>
                     </SelectContent>
                   </Select>
