5f9f89d8cb113620865201f0f34aac10a7f33209
WalidTawfik1 <walidtawfik362@gmail.com>
2025-07-10 04:19:02 +0300

    feat: Add sitemap index generation and update routing in vercel.json and robots.txt

---
+++ api/sitemap-index.js
@@ -0,0 +1,24 @@
+export default function handler(req, res) {
+  // Only allow GET requests
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  // Set headers for XML content and caching
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, max-age=3600, s-maxage=3600');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('X-Robots-Tag', 'noindex');
+  
+  // Generate sitemap index XML
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemap>
+<loc>https://elcentre-learn.vercel.app/sitemap.xml</loc>
+<lastmod>2025-07-10</lastmod>
+</sitemap>
+</sitemapindex>`;
+
+  res.status(200).end(sitemapIndex);
+}
